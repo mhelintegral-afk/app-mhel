1873bc3b04220fd05d9eb622b5a017fd0a6c76a3
mhelintegral-afk <mhel.integral@gmail.com>
2026-04-11 14:43:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "mhel-app-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
